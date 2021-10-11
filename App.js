3b2777b398c82a47bb2276361de592eca8d3f6b5
BuildTools <unconfigured@null.spigotmc.org>
2021-10-11 11:31:28 +0900

    naverid bug

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 document.getElementById("naverid").addEventListener("click", (event) => {
-    window.open('https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=d38d7PSWS1ORhJ8PiwvJ&state=tocken&redirect_uri=https%3a%2f%2fpapago1.netlify.app%2floginok',"","_blank");
-
+    location.href = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=d38d7PSWS1ORhJ8PiwvJ&state=tocken&redirect_uri=https%3a%2f%2fpapago1.netlify.app%2floginok"
   });
   
